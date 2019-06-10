82a1bbd54f016124069558e716bd8e5d34195180
Daniel Crewdson <daniel.crewdson@dewsign.co.uk>
2019-06-10 10:29:58 +0100

    initial film data n stuff

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container mx-auto px-4">
+        <ul class="flex flex-wrap">
+            <li v-for="film in films" class="flex-1 text-center w-1/3 px-4 py-2 m-2 border border-teal-200 rounded bg-grey-300">
+                <h2 class="">
+                    {{ film.title }}
+                </h2>
+                <div class="text-gray-500">
+                    {{ film.release_date }}
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+
+    data() {
+        return {
+            films: [],
+        }
+    },
+
+    mounted() {
+        this.$axios.get('films')
+            .then((res) => {
+                this.films = res.data
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+    }
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/tailwind.css'
+
+import 'nprogress/nprogress.css'
+import http from './api'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$axios = http
 
 new Vue({
   render: h => h(App),
